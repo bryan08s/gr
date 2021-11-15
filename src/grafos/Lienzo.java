79567762f8b40ed0b7962adc17d8e779be7441b4
UrieeelH <94318221+UrieeelH@users.noreply.github.com>
2021-11-15 15:43:45 -0600

    Add files via upload

--- src/grafos/Lienzo.java
+++ src/grafos/Lienzo.java
@@ -1,54 +1,69 @@
-package grafos;
+package Random;
+
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Vector;
 import javax.swing.JPanel;
-/**
- *
- * @author Bryan Saim
- */
+
 public class Lienzo extends JPanel implements MouseListener{
-     private Vector<Nodo> vectorNodos;
+    private Vector <Nodo> vectorNodos;
+    private Vector<Enlace> vectorEnlaces;
+    private Point p1,p2;
     
     public Lienzo(){
-        this.vectorNodos=new Vector<>();
-        this.addMouseListener(this);
-    }
-    
-    @Override
+    this.vectorNodos=new Vector<>();
+    this.vectorEnlaces=new Vector<>();
+    this.addMouseListener(this);
+}
+
     public void paint(Graphics g){
-        for(Nodo nodos: vectorNodos){
-            nodos.Pintar(g);
+        for(Nodo nodos:vectorNodos){
+            nodos.pintar(g);
+            for(Enlace enlace:vectorEnlaces){
+                enlace.pintar(g);
+            }
         }
     }
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(e.getButton() == MouseEvent.BUTTON1){
-            this.vectorNodos.add(new Nodo(e.getX(), e.getY()));
-            repaint();
+        if (e.getButton()== MouseEvent.BUTTON1){
+            this.vectorNodos.add(new Nodo(e.getX(),e.getY()));
+            repaint();}
+        if (e.getButton()==MouseEvent.BUTTON3){
+            for(Nodo nodo: vectorNodos){
+                if(new Rectangle(nodo.getX()-Nodo.d/2,nodo.getY()-Nodo.d/2,Nodo.d,Nodo.d).contains(e.getPoint())){
+                    if(p1==null){
+                        p1=new Point(nodo.getX(),nodo.getY());
+                    }
+                    else{
+                        p2=new Point(nodo.getX(),nodo.getY());
+                        this.vectorEnlaces.add(new Enlace(p1.x,p1.y,p2.x,p2.y));
+                        repaint();
+                        p1=null;
+                        p2=null;
+                    }
+                }
+            }
         }
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
-        
+    public void mousePressed(MouseEvent me) {
     }
 
     @Override
     public void mouseReleased(MouseEvent me) {
-        
     }
 
     @Override
     public void mouseEntered(MouseEvent me) {
-        
     }
 
     @Override
     public void mouseExited(MouseEvent me) {
-        
     }
-    
     
 }
